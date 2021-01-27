e19d505cb6e8e94be0414b0cd2efd7f707e2824b
kmcooper <kmcooper@unomaha.edu>
2021-01-27 15:25:17 -0600

    Update testScript.sql

--- testScript.sql
+++ testScript.sql
@@ -1,8 +1,11 @@
 -- Change the netid value to reflect yours
 USE netid;
 
+-- Drop the dna_records table if it already exists
+DROP TABLE dna_records_1;
+
 -- DDL to create table
-CREATE TABLE dna_records(
+CREATE TABLE dna_records_1(
     record_id INT,
     geneName VARCHAR(30),
     geneSymbol VARCHAR(10),
@@ -11,8 +14,8 @@ CREATE TABLE dna_records(
 
 -- DML to insert some data
 -- Note the data below is not necessarily accurate
-INSERT INTO dna_records(1,'tumor suppresor protein 53', 'TP53', 'homo sapiens');
-INSERT INTO dna_records(2,'kirstan ras oncogene homolog', 'KRAS', 'mus musculus');
-INSERT INTO dna_records(3,'MYC proto-oncogene, bHLH transcription factor', 'c-myc', 'homo sapiens');
-INSERT INTO dna_records(4,'klotho', 'KL', 'mus musculus');
-INSERT INTO dna_records(4,'titin protein', 'TTN', 'homo sapiens');
+INSERT INTO dna_records_1 VALUES (1,'tumor suppresor protein 53', 'TP53', 'homo sapiens');
+INSERT INTO dna_records_1 VALUES (2,'kirstan ras oncogene homolog', 'KRAS', 'mus musculus');
+INSERT INTO dna_records_1 VALUES (3,'MYC proto-oncogene, bHLH transcription factor', 'c-myc', 'homo sapiens');
+INSERT INTO dna_records_1 VALUES (4,'klotho', 'KL', 'mus musculus');
+INSERT INTO dna_records_1 VALUES (4,'titin protein', 'TTN', 'homo sapiens');

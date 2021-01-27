7b81d314898e51e46d3e1a303e6d1ab45745d1d9
kmcooper <kmcooper@unomaha.edu>
2021-01-27 15:27:16 -0600

    Update testScript.sql

--- testScript.sql
+++ testScript.sql
@@ -9,7 +9,8 @@ CREATE TABLE dna_records_1(
     record_id INT,
     geneName VARCHAR(30),
     geneSymbol VARCHAR(10),
-    organism VARCHAR(30)
+    organism VARCHAR(30),
+    PRIMARY KEY (record_id)
 );
 
 -- DML to insert some data
@@ -18,4 +19,4 @@ INSERT INTO dna_records_1 VALUES (1,'tumor suppresor protein 53', 'TP53', 'homo 
 INSERT INTO dna_records_1 VALUES (2,'kirstan ras oncogene homolog', 'KRAS', 'mus musculus');
 INSERT INTO dna_records_1 VALUES (3,'MYC proto-oncogene, bHLH tf', 'c-myc', 'homo sapiens');
 INSERT INTO dna_records_1 VALUES (4,'klotho', 'KL', 'mus musculus');
-INSERT INTO dna_records_1 VALUES (4,'titin protein', 'TTN', 'homo sapiens');
+INSERT INTO dna_records_1 VALUES (5,'titin protein', 'TTN', 'homo sapiens');

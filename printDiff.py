38710a5dd70a06476795684aec64d43e4d4158b1
Kate Cooper | kmcooper <kmcooper@unomaha.edu>
2023-12-28 10:54:14 -0600

    Create printDiff.py

---
+++ printDiff.py
@@ -0,0 +1,4 @@
+print("There\tare\ttabs\tbetween\twords\ton\tthis\tline\n")
+print("There    are tabs    between words   on  this    line    but they    are not explicit\n")
+print("There are spaces between words on this line\n")
+print("There\tis a mixture of tabs\tand\tspaces\tbetween\twords\ton this line\n") 
